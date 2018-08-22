692b714b5b146bf75e33f3c160934db2a8437544
shi Defeng <shi.defeng@immomo.com>
2018-08-22 20:06:18 +0800

    完善EGL配置

--- app/src/main/java/com/example/momo/glesdemo/MainActivity.java
+++ app/src/main/java/com/example/momo/glesdemo/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.momo.glesdemo;
 
+import android.opengl.GLES20;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import com.example.momo.glesdemo.gles.EGL14Wrapper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,14 +19,87 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initView();
+
     }
 
     private void initView() {
+        final SurfaceView surfaceView = (SurfaceView) findViewById(R.id.surface_view);
+        final SurfaceHolder holder = surfaceView.getHolder();
+        holder.addCallback(new SurfaceHolder.Callback() {
 
+            private RenderThread mRenderThread;
+
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                mRenderThread = new RenderThread(holder.getSurface());
+                mRenderThread.start();
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                mRenderThread.exit();
+            }
+        });
     }
 
-    private void initEgl() {
+    private static class RenderThread extends Thread {
 
+        private EGL14Wrapper mEglScreen;
+        private Surface mSurface;
+
+        private volatile boolean mRunning = true;
+        private volatile long mLastTime = 0L;
+
+        private RenderThread(Surface surface) {
+            mSurface = surface;
+        }
+
+        @Override
+        public void run() {
+            ensureEgl(mSurface);
+            while (mRunning) {
+                try {
+                    Thread.sleep(16);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                mEglScreen.makeCurrent();
+                render();
+                mEglScreen.swapBuffer();
+            }
+
+            releaseEgl();
+        }
+
+        public void exit() {
+            mRunning = false;
+        }
+
+        private void ensureEgl(Surface surface) {
+            if (mEglScreen == null) {
+                mEglScreen = new EGL14Wrapper();
+                mEglScreen.createScreenEgl(surface);
+            }
+        }
+
+        private void render() {
+            final long start = SystemClock.uptimeMillis();
+            Log.i("forTest", "render used time:" + (start - mLastTime));
+            mLastTime = start;
+            GLES20.glClearColor(1f, 0f, 0f, 1f);
+            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        }
+
+        private void releaseEgl() {
+            if (mEglScreen != null) {
+                mEglScreen.release();
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/momo/glesdemo/gles/EGL14Wrapper.java
+++ app/src/main/java/com/example/momo/glesdemo/gles/EGL14Wrapper.java
@@ -1,32 +1,136 @@
 package com.example.momo.glesdemo.gles;
 
 import android.opengl.EGL14;
+import android.opengl.EGLConfig;
+import android.opengl.EGLContext;
 import android.opengl.EGLDisplay;
+import android.opengl.EGLSurface;
 import android.opengl.GLUtils;
+import android.util.Log;
 
 /**
- * @author feng
+ * @author shidefeng
  * @since 2018/8/20.
  */
-
 public class EGL14Wrapper {
 
-    public void createScreenEgl() {
-        // 第一步：
-        final EGLDisplay mEglDisplay = EGL14.eglGetDisplay(EGL14.EGL_DEFAULT_DISPLAY);
-        if (mEglDisplay == EGL14.EGL_NO_DISPLAY || EGL14.eglGetError() != EGL14.EGL_SUCCESS){
-            throw new RuntimeException("eglGetDisplay, failed " + GLUtils.getEGLErrorString(EGL14.eglGetError()));
+    private EGLDisplay mEglDisplay;
+    private EGLConfig mEglConfig;
+    private EGLContext mEglContext;
+    private EGLSurface mEglSurface;
+
+    public void createScreenEgl(Object surface) {
+        // 第一步：关联显示设备，mEglDisplay相当于显示器的句柄
+        mEglDisplay = EGL14.eglGetDisplay(EGL14.EGL_DEFAULT_DISPLAY);
+        Log.i("forTest", "eglDisplay:" + mEglDisplay);
+
+        // 判断系统中是否有可用的 native display ID 与给定的 display 参数匹配
+        if (mEglDisplay == EGL14.EGL_NO_DISPLAY || EGL14.eglGetError() != EGL14.EGL_SUCCESS) {
+            throw new RuntimeException("eglGetDisplay, failed:" + GLUtils.getEGLErrorString(EGL14.eglGetError()));
         }
 
-        // 第二步：
-        final int[] versions = new int[2];
-        if (!EGL14.eglInitialize(mEglDisplay, versions, 0, versions, 1)) {
-            throw new RuntimeException("eglInitialize, failed " + GLUtils.getEGLErrorString(EGL14.eglGetError()));
+        // 第二步：每个 EGLDisplay 在使用前都需要初始化。初始化当前的Display
+        // 初始化 EGLDisplay 的同时，你可以得到系统中 EGL 的实现版本号。
+        final int[] version = new int[2];
+        if (!EGL14.eglInitialize(mEglDisplay, version, 0, version, 1)) {
+            throw new RuntimeException("eglInitialize, failed:" + GLUtils.getEGLErrorString(EGL14.eglGetError()));
+        }
+        Log.i("forTest", "version:" + version[0] + "," + version[1]);
+
+        // 第三步：获取、配置config
+        final int configsCount[] = new int[1];
+        final EGLConfig configs[] = new EGLConfig[1];
+        int configSpec[] = /*isAlpha ? new int[]{
+                EGL14.EGL_RENDERABLE_TYPE, EGL14.EGL_OPENGL_ES2_BIT,
+                EGL14.EGL_RED_SIZE, 8,
+                EGL14.EGL_GREEN_SIZE, 8,
+                EGL14.EGL_BLUE_SIZE, 8,
+                EGL14.EGL_ALPHA_SIZE, 8,
+                EGL14.EGL_DEPTH_SIZE, 16,
+                EGL14.EGL_STENCIL_SIZE, 0,
+                EGL14.EGL_NONE
+        } : */{
+                EGL14.EGL_RENDERABLE_TYPE, EGL14.EGL_OPENGL_ES2_BIT,
+                EGL14.EGL_RED_SIZE, 8,
+                EGL14.EGL_GREEN_SIZE, 8,
+                EGL14.EGL_BLUE_SIZE, 8,
+                EGL14.EGL_DEPTH_SIZE, 0,
+                EGL14.EGL_STENCIL_SIZE, 0,
+                EGL14.EGL_NONE
+        };
+        // attrib_list：选择配置时需要参照的属性
+        // configs：返回一个按照 attrib_list 排序的平台有效的所有 EGL framebuffer 配置列表
+        // config_size：手动设置应返回的配置个数
+        // num_config：实际匹配的配置总数
+        if (!EGL14.eglChooseConfig(mEglDisplay, configSpec, 0, configs, 0, 1, configsCount, 0)
+                || configsCount[0] <= 0) {
+            throw new RuntimeException("eglChooseConfig, failed:" + GLUtils.getEGLErrorString(EGL14.eglGetError()));
         }
 
-        // 第三步：
+        mEglConfig = configs[0];
+        Log.i("forTest", "eglChooseConfig:" + mEglConfig);
 
+        // 第四步：构建surface
+        final int[] surface_attr = {
+                EGL14.EGL_NONE
+        };
+        mEglSurface = EGL14.eglCreateWindowSurface(mEglDisplay, mEglConfig, surface, surface_attr, 0);
+        if (mEglSurface == null || mEglSurface == EGL14.EGL_NO_SURFACE) {
+            throw new RuntimeException("eglCreateWindowSurface, failed:" + GLUtils.getEGLErrorString(EGL14.eglGetError()));
+        }
 
+        Log.i("forTest", "mEglSurface:" + mEglSurface);
+
+        // 第五步：创建Context
+        final int[] context_attr = {
+                EGL14.EGL_CONTEXT_CLIENT_VERSION, 2,
+                EGL14.EGL_NONE
+        };
+        mEglContext = EGL14.eglCreateContext(mEglDisplay, mEglConfig, EGL14.EGL_NO_CONTEXT, context_attr, 0);
+        if (mEglContext == EGL14.EGL_NO_CONTEXT) {
+            throw new RuntimeException("eglCreateContext, failed:" + GLUtils.getEGLErrorString(EGL14.eglGetError()));
+        }
+    }
+
+    public boolean makeCurrent() {
+        if (mEglDisplay != null && mEglSurface != null && mEglContext != null) {
+            if (!EGL14.eglMakeCurrent(mEglDisplay, mEglSurface, mEglSurface, mEglContext)) {
+                throw new RuntimeException("eglMakeCurrent, failed:" + GLUtils.getEGLErrorString(EGL14.eglGetError()));
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean makeCurrent(EGLSurface readSurface) {
+        if (mEglDisplay != null && mEglSurface != null && mEglContext != null) {
+            if (!EGL14.eglMakeCurrent(mEglDisplay, mEglSurface, readSurface, mEglContext)) {
+                throw new RuntimeException("eglMakeCurrent, failed:" + GLUtils.getEGLErrorString(EGL14.eglGetError()));
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void swapBuffer() {
+        if (mEglDisplay != null && mEglSurface != null) {
+            if (!EGL14.eglSwapBuffers(mEglDisplay, mEglSurface)) {
+                throw new RuntimeException("eglSwapBuffers,failed!");
+            }
+        }
+    }
+
+    public void release() {
+        // null everything out so future attempts to use this object will cause an NPE
+        if (mEglDisplay != null && mEglSurface != null && mEglContext != null) {
+            EGL14.eglMakeCurrent(this.mEglDisplay, this.mEglSurface, this.mEglSurface, this.mEglContext);
+            EGL14.eglDestroySurface(mEglDisplay, mEglSurface);
+            EGL14.eglDestroyContext(mEglDisplay, mEglContext);
+            EGL14.eglTerminate(mEglDisplay);
+        }
+        EGL14.eglMakeCurrent(mEglDisplay, EGL14.EGL_NO_SURFACE, EGL14.EGL_NO_SURFACE, EGL14.EGL_NO_CONTEXT);
     }
 
 }
